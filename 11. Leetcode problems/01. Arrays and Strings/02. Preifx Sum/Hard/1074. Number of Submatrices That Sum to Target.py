2a2a831676ca30ee84f6c6e745c05b1671c4273a
kom3 <manjunathkankatteko@gmail.com>
2026-01-01 23:53:26 +0530

    .

--- 11. Leetcode problems/01. Arrays and Strings/02. Preifx Sum/Hard/1074. Number of Submatrices That Sum to Target.py
+++ 11. Leetcode problems/01. Arrays and Strings/02. Preifx Sum/Hard/1074. Number of Submatrices That Sum to Target.py
@@ -10,15 +10,21 @@
 # Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
 # Output: 4
 # Explanation: The four 1x1 submatrices that only contain 0.
-# Example 2:
 
+# Example 2:
 # Input: matrix = [[1,-1],[-1,1]], target = 0
 # Output: 5
 # Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
-# Example 3:
 
+# Example 3:
 # Input: matrix = [[904]], target = 0
 # Output: 0
+
+# Constraints:
+# 1 <= matrix.length <= 100
+# 1 <= matrix[0].length <= 100
+# -1000 <= matrix[i][j] <= 1000
+# -10^8 <= target <= 10^8
 
 
 
