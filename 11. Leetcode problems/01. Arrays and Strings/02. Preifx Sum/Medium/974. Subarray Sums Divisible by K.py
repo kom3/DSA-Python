915a6126f710f328b91bc51dda0bb1726da28430
kom3 <manjunathkankatteko@gmail.com>
2026-01-01 22:33:14 +0530

    .

--- 11. Leetcode problems/01. Arrays and Strings/02. Preifx Sum/Medium/974. Subarray Sums Divisible by K.py
+++ 11. Leetcode problems/01. Arrays and Strings/02. Preifx Sum/Medium/974. Subarray Sums Divisible by K.py
@@ -26,3 +26,39 @@
 
 
 
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        """
+        Count the number of subarrays divisible by k.
+        Pattern formula: i - j = length
+        Subarray [j+1 ... i] is divisible by k if:
+            prefix[i] % k == prefix[j] % k
+        """
+
+        # frequency map for prefix_sum % k
+        remainder_freq = {0: 1}  # remainder 0 seen once (empty prefix)
+        prefix_sum = 0
+        count = 0
+
+        for num in nums:
+            prefix_sum += num
+
+            # calculate normalized remainder
+            # why normalized reminder?
+            # In Python: -1 % 5  # → 4 So it’s usually safe. But in general, for clarity and safety, it’s best to normalize the remainder.
+            # In other languages like C++ or Java, modulo with negative numbers works differently:
+            # -1 % 5  // → -1 in C++
+            # -2 % 5  → -2
+            rem = (prefix_sum % k + k) % k  # handles negative numbers
+
+            # add count of previous subarrays with same remainder
+            count += remainder_freq.get(rem, 0)
+
+            # update remainder frequency
+            remainder_freq[rem] = remainder_freq.get(rem, 0) + 1
+
+        return count
+
+
+
